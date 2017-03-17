0e7e479246a309316760e360d4586b9080dbacf2
JING ZHU <gin@timezenstudio.com>
2017-03-18 01:45:55 +0800

    Polish Cocktails CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
