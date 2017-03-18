2edfc8069042672c1f03d31e59f18f4d1a2fbdf6
Xindong Ding <dxd.spirits@gmail.com>
2017-03-19 04:59:02 +0800

    validate dose form. ingredient_id and description is required (presence: true)

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :description, presence: true
+  validates :ingredient_id, presence: true
 end
